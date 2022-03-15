84619e828ef1e08a406674d4439e661b9291ba42
Jasmine Nahorniak <jasmine.nahorniak@oregonstate.edu>
2022-03-15 16:57:40 -0700

    Adds username to deployment example.

--- deploy_example.sh
+++ deploy_example.sh
@@ -6,5 +6,5 @@ remote_dir="/home/pi/nav"
 for i in 0{1..2}; do
   host="navbeacon$i"
   echo "Deploying to $host"
-  rsync -ra --partial-dir=/tmp --files-from=deploy_list.txt "$local_dir" "$host:$remote_dir"
+  rsync -ra --partial-dir=/tmp --files-from=deploy_list.txt "$local_dir" "pi@$host:$remote_dir"
 done
